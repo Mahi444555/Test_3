f4a3a7461a2372ea27fc5e5a22844178f3b5b91d
abc <abc@gmail.com>
2022-05-26 16:48:30 +0530

    edited form local machine

--- Matrix/2_program.cpp
+++ Matrix/2_program.cpp
@@ -1,9 +1,13 @@
 /*program for print following pattern
-1               1
-1 2           2 1  
-1 2 3       3 2 1 
-1 2 3 4   4 3 2 1 
 1 2 3 4 5 4 3 2 1
+1 2 3 4   4 3 2 1 
+1 2 3       3 2 1
+1 2           2 1
+1               1
+  
+ 
+
+
 */
 #include<iostream>
 using namespace std;
